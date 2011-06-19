6a54a1f344ed5ec58f79a2600ae424752d01f083
Mike David <mikedavid@gaikai.com>
2011-06-18 18:46:14 -0700

    pyhard first commit

---
+++ ex1extra2.py
@@ -0,0 +1,4 @@
+#this is a way i can describe the line below, so if anyone else reads it, 
+#they'll know i'm still a beginner and thats why it doesn't make sense. :)
+print 'Yay! Printing.'
+#lanaaay is my favorite.
